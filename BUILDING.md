22f8b7162f926a75379875e4992e7aee2d9684b1
Luke Nihlen <luke.nihlen@gmail.com>
2019-03-23 14:41:03 -0700

    glfw and vulkan life signs

---
+++ BUILDING.md
@@ -0,0 +1,5 @@
+Putting some basic notes about building stuff here, will organize into a more
+formal guide later.
+
+sudo apt-get install libvulkan-dev libxinerama-dev libxcursor-dev vulkan-headers vulkan-validationlayers libxi-dev
+
---
+++ third_party/oscpack_build.cc
@@ -0,0 +1,9 @@
+#define OSC_HOST_LITTLE_ENDIAN
+
+#include "oscpack_1_1_0/ip/posix/NetworkingUtils.cpp"
+#include "oscpack_1_1_0/ip/posix/UdpSocket.cpp"
+
+#include "oscpack_1_1_0/osc/OscOutboundPacketStream.cpp"
+#include "oscpack_1_1_0/osc/OscPrintReceivedElements.cpp"
+#include "oscpack_1_1_0/osc/OscReceivedElements.cpp"
+#include "oscpack_1_1_0/osc/OscTypes.cpp"
