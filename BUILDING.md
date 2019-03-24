8eb3d3e1f9c5fbdec5a1df4a8b0761243d4232f0
Luke Nihlen <luke.nihlen@gmail.com>
2019-03-24 12:03:50 -0700

    adds vulkan validation layers as build option

--- BUILDING.md
+++ BUILDING.md
@@ -1,5 +1,6 @@
 Putting some basic notes about building stuff here, will organize into a more
 formal guide later.
 
-sudo apt-get install libvulkan-dev libxinerama-dev libxcursor-dev vulkan-headers vulkan-validationlayers libxi-dev
+sudo apt-get install libvulkan-dev libxinerama-dev libxcursor-dev \
+    vulkan-headers vulkan-validationlayers libxi-dev libxrandr-dev
 
--- src/scinsynth.cc
+++ src/scinsynth.cc
@@ -1,41 +1,170 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-/*
-
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
-
-*/
-
 #include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+#include <cstring>
+#include <vector>
 
-int main() {
-    glfwInit();
+const int WIDTH = 800;
+const int HEIGHT = 600;
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+#if defined(SCIN_VALIDATE_VULKAN)
+VkResult CreateDebugUtilsMessengerEXT(
+  VkInstance instance,
+  const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
+  const VkAllocationCallbacks* pAllocator,
+  VkDebugUtilsMessengerEXT* pDebugMessenger) {
+  auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(
+      instance, "vkCreateDebugUtilsMessengerEXT");
+  if (func != nullptr) {
+      return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
+  } else {
+      return VK_ERROR_EXTENSION_NOT_PRESENT;
+  }
+}
 
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+void DestroyDebugUtilsMessengerEXT(
+  VkInstance instance,
+  VkDebugUtilsMessengerEXT debugMessenger,
+  const VkAllocationCallbacks* pAllocator) {
+  auto func = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(
+      instance, "vkDestroyDebugUtilsMessengerEXT");
+  if (func != nullptr) {
+      func(instance, debugMessenger, pAllocator);
+  }
+}
 
-    std::cout << extensionCount << " extensions supported" << std::endl;
+const std::vector<const char*> validationLayers = {
+    "VK_LAYER_LUNARG_standard_validation"
+};
 
-/*
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
-*/
+bool CheckValidationLayerSupport() {
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
 
-    while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+    for (const char* layerName : validationLayers) {
+        bool layerFound = false;
+
+        for (const auto& layerProperties : availableLayers) {
+            if (strcmp(layerName, layerProperties.layerName) == 0) {
+                layerFound = true;
+                break;
+            }
+        }
+
+        if (!layerFound) {
+            return false;
+        }
     }
 
-    glfwDestroyWindow(window);
+    return true;
+}
 
-    glfwTerminate();
+static VKAPI_ATTR VkBool32 VKAPI_CALL DebugCallback(
+    VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+    VkDebugUtilsMessageTypeFlagsEXT messageType,
+    const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+    void* pUserData) {
+  std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+  return VK_FALSE;
+}
 
-    return 0;
+bool SetupDebugMessenger(VkInstance instance,
+    VkDebugUtilsMessengerEXT* debugMessenger) {
+  VkDebugUtilsMessengerCreateInfoEXT createInfo = {};
+  createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+  createInfo.messageSeverity =
+      VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |
+      VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+      VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+  createInfo.messageType =
+      VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+      VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+      VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+  createInfo.pfnUserCallback = DebugCallback;
+
+  if (CreateDebugUtilsMessengerEXT(instance, &createInfo, nullptr,
+      debugMessenger) != VK_SUCCESS) {
+      return false;
+    }
+
+  return true;
+}
+#endif
+
+int main() {
+  // glfw setup and window creation.
+  glfwInit();
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+  glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+  GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT,
+      "ScintillatorSynth", nullptr, nullptr);
+
+  // Vulkan setup.
+  VkApplicationInfo appInfo = {};
+  appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+  appInfo.pApplicationName = "scinsynth";
+  appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+  appInfo.pEngineName = "Scintillator";
+  appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+  appInfo.apiVersion = VK_API_VERSION_1_0;
+
+  VkInstanceCreateInfo createInfo = {};
+  createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+  createInfo.pApplicationInfo = &appInfo;
+
+  uint32_t glfwExtensionCount = 0;
+  const char** glfwExtensions;
+  glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+  std::vector<const char*> extensions(glfwExtensions,
+      glfwExtensions + glfwExtensionCount);
+
+#if defined(SCIN_VALIDATE_VULKAN)
+  extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+  createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+  createInfo.ppEnabledLayerNames = validationLayers.data();
+#else
+  createInfo.enabledLayerCount = 0;
+#endif
+
+  createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+  createInfo.ppEnabledExtensionNames = extensions.data();
+
+  VkInstance instance;
+  if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+    std::cerr << "failed to create instance!" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+#if defined(SCIN_VALIDATE_VULKAN)
+  VkDebugUtilsMessengerEXT debugMessenger;
+  if (!SetupDebugMessenger(instance, &debugMessenger)) {
+    std::cerr << "failed to create debug messenger" << std::endl;
+    return EXIT_FAILURE;
+  }
+#endif
+
+  // Main loop.
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+  }
+
+#if defined(SCIN_VALIDATE_VULKAN)
+  DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
+#endif
+
+  // Vulkan cleanup.
+  vkDestroyInstance(instance, nullptr);
+
+  // glfw cleanup.
+  glfwDestroyWindow(window);
+  glfwTerminate();
+
+  return EXIT_SUCCESS;
 }
