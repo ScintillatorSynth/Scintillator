4d9b4be9747bc8fe815eb5b50c3805f9c1125b83
Luke Nihlen <luke.nihlen@gmail.com>
2019-03-25 13:13:14 -0700

    adjust whitespace to 4 spaces to reflect SuperCollider style

--- src/scinsynth.cc
+++ src/scinsynth.cc
@@ -16,46 +16,46 @@ const int HEIGHT = 600;
 
 
 int main() {
-  // ========== glfw setup and window creation.
-  glfwInit();
-  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-  glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-  GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT,
-      "ScintillatorSynth", nullptr, nullptr);
+        // ========== glfw setup and window creation.
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT,
+                        "ScintillatorSynth", nullptr, nullptr);
 
-  // ========== Vulkan setup.
-  scin::VulkanInstance vk_instance;
-  if (!vk_instance.Create()) {
-    return EXIT_FAILURE;
-  }
+        // ========== Vulkan setup.
+        scin::VulkanInstance vk_instance;
+        if (!vk_instance.Create()) {
+                return EXIT_FAILURE;
+        }
 
-  // Create Vulkan window surface.
-  VkSurfaceKHR surface;
-  if (glfwCreateWindowSurface(vk_instance.get(), window, nullptr, &surface)
-      != VK_SUCCESS) {
-    std::cerr << "failed to create surface" << std::endl;
-    return EXIT_FAILURE;
-  }
+        // Create Vulkan window surface.
+        VkSurfaceKHR surface;
+        if (glfwCreateWindowSurface(vk_instance.get(), window, nullptr, &surface)
+                        != VK_SUCCESS) {
+                std::cerr << "failed to create surface" << std::endl;
+                return EXIT_FAILURE;
+        }
 
-  // Create Vulkan logical device.
-  scin::VulkanDevice vk_device(&vk_instance);
-  if (!vk_device.Create(surface)) {
-    return EXIT_FAILURE;
-  }
+        // Create Vulkan logical device.
+        scin::VulkanDevice vk_device(&vk_instance);
+        if (!vk_device.Create(surface)) {
+                return EXIT_FAILURE;
+        }
 
-  // ========== Main loop.
-  while (!glfwWindowShouldClose(window)) {
-    glfwPollEvents();
-  }
+        // ========== Main loop.
+        while (!glfwWindowShouldClose(window)) {
+                glfwPollEvents();
+        }
 
-  // ========== Vulkan cleanup.
-  vk_device.Destroy();
-  vkDestroySurfaceKHR(vk_instance.get(), surface, nullptr);
-  vk_instance.Destroy();
+        // ========== Vulkan cleanup.
+        vk_device.Destroy();
+        vkDestroySurfaceKHR(vk_instance.get(), surface, nullptr);
+        vk_instance.Destroy();
 
-  // ========== glfw cleanup.
-  glfwDestroyWindow(window);
-  glfwTerminate();
+        // ========== glfw cleanup.
+        glfwDestroyWindow(window);
+        glfwTerminate();
 
-  return EXIT_SUCCESS;
+        return EXIT_SUCCESS;
 }
