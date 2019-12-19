df14e7cce32a0fee5ac2355972468b385305714e
Luke Nihlen <luke.nihlen@gmail.com>
2019-12-18 16:51:40 -0800

    document VGenManager, add docs to deployment

--- src/VGenManager.hpp
+++ src/VGenManager.hpp
@@ -14,26 +14,58 @@ namespace scin {
 
 class VGen;
 
-// Manages VGens, can parse them from YAML, keeps the master list of them.
+/*! Responsible for the parsing, storage, retrieval, and updating of VGen objects.
+ *
+ * VGenManager is intended to keep the authoritative list of all VGen objects known by the system. It supports
+ * multi-threaded access at the cost of a mutex acquisition per access. It supports uniquely identifying VGens by name.
+ * Adding a new VGen with the same name as one already registered with VGenManager will result in the old VGen being
+ * overwritten.
+ */
 class VGenManager {
 public:
     VGenManager();
     ~VGenManager();
 
-    // Convenience routine when the VGen yaml is in a file (mostly for the built-in VGens). Returns the number of
-    // VGens parsed, or a negative number on error.
+    /*! Parse VGens from the supplied yaml file.
+     *
+     * \param fileName A path and filename of the file to parse and load.
+     * \return The number of valid VGens parsed, or a negative number on load error.
+     */
     int loadFromFile(const std::string& fileName);
-    // Parse a VGen from a string.
+
+    /*! Parse VGens from a yaml string.
+     *
+     * \param yaml The string containing one or more VGen yaml documents to parse and load.
+     * \return The number of valid VGens parsed, or a negative number on parse error.
+     */
     int parseFromString(const std::string& yaml);
 
-    // mutex hit
+    /*! Lookup a VGen by name and return if found.
+     *
+     * \param name The unique name of the VGen to find.
+     * \return A pointer to the VGen object found, or nullptr if no object found under that name.
+     */
     std::shared_ptr<VGen> getVGenNamed(const std::string& name);
-    // mutex hit
+
+    /*! The size of the current VGen dictionary.
+     *
+     * \return The number of unique VGens currently loaded and validated by the system.
+     */
     size_t numberOfVGens();
 
 private:
+    /*! Pulls individual VGen objects from the parsed yaml data structures and stores them in the dictionary.
+     *
+     * \param nodes The vector of nodes extracted from the file or string.
+     * \return The number of valid VGens extracted.
+     */
     int extractFromNodes(const std::vector<YAML::Node>& nodes);
-    // Extracts a single VGen from a parsed YAML node.
+
+    /*! Pull an single VGen from the parsed yaml data structure and store it in the dictionary.
+     *
+     * \param node The yaml node that is considered to be a VGen.
+     * \return true if node was a valid VGen, false on error.
+     */
     bool extractFromNode(YAML::Node& node);
 
     std::mutex m_mutex;
