a89a0588971e00c87418a28f929a57fcdc07442b
Luke Nihlen <luke.nihlen@gmail.com>
2020-03-20 17:09:36 -0700

    answer yes to sc ppa

--- .travis/before-install-linux.sh
+++ .travis/before-install-linux.sh
@@ -5,7 +5,7 @@ wget -qO - http://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo apt-key 
 sudo wget -qO /etc/apt/sources.list.d/lunarg-vulkan-bionic.list http://packages.lunarg.com/vulkan/lunarg-vulkan-bionic.list
 # Add the SuperCollider PPA
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys FABAEF95
-sudo add-apt-repository ppa:supercollider/ppa
+sudo add-apt-repository --yes ppa:supercollider/ppa
 sudo apt-get update
 # Install all the build and test dependencies
 sudo apt-get install --yes build-essential clang-8 clang-format-8 cmake doxygen glslang-dev gperf imagemagick          \
