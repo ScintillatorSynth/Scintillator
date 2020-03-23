c7dc3a76f9eecf13105839112784f2c1fc4786a4
Luke Nihlen <luke.nihlen@gmail.com>
2020-03-22 22:20:43 -0700

    clean up dep builds

--- .travis/before-install-osx.sh
+++ .travis/before-install-osx.sh
@@ -2,34 +2,13 @@
 
 export HOMEBREW_NO_ANALYTICS=1
 
-# SuperCollider build dependencies
 brew update
-brew install libsndfile || brew install libsndfile || exit 1
-brew install portaudio || exit 2
-brew install ccache || exit 3
-brew link qt5 --force || exit 5
+brew unlink python@2
+brew install ccache doxygen shtool supercollider
 
 # according to https://docs.travis-ci.com/user/caching#ccache-cache
 export PATH="/usr/local/opt/ccache/libexec:$PATH"
 
 # To get less noise in xcode output, as some builds are terminated for exceeding maximum log length.
 gem install xcpretty-travis-formatter
-
-# Checkout and build SuperCollider into $HOME/bin
-mkdir $HOME/bin
-mkdir $HOME/src
-cd src
-git clone --recursive https://github.com/supercollider/supercollider
-cd supercollider
-mkdir build
-cd build
-cmake -G Xcode -DCMAKE_INSTALL_PREFIX=$HOME/bin -DCMAKE_PREFIX_PATH=`brew --prefix qt5` -DSC_ABLETON_LINK=OFF          \
-    -DSC_EL=OFF ..
-cmake --build . --target install --config RelWithDebInfo
-
-cd $TRAVIS_BUILD_DIR
-
-# Install Scintillator buildtime dependencies
-brew unlink python@2
-brew install ninja doxygen lame libass shtool texi2html nasm
 
