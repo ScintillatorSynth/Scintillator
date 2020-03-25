1a86d2fc2157d13918595a3796fe2fc85bf77236
Luke Nihlen <luke.nihlen@gmail.com>
2020-03-25 12:54:49 -0700

    travis cache errors

--- .travis/before-script-osx.sh
+++ .travis/before-script-osx.sh
@@ -2,6 +2,5 @@
 
 cd $TRAVIS_BUILD_DIR/build
 
-cmake -G Xcode -DPYTHON_EXECUTABLE=`which python3` -DCMAKE_OSX_DEPLOYMENT_TARGET=10.10                                 \
-    -DSCIN_SCLANG=$HOME/bin/SuperCollider/SuperCollider.app/Contents/MacOS/sclang ..
+cmake -G Xcode -DPYTHON_EXECUTABLE=`which python3` ..
 
--- .travis/script-osx.sh
+++ .travis/script-osx.sh
@@ -2,6 +2,8 @@
 
 XCPRETTY='xcpretty --simple --no-utf --no-color'
 
+pwd
+
 cmake --build . --config Release | $XCPRETTY
 cmake --install .
 
