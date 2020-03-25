8c63e3a28abdbc063bca07f61fb20f1eb05a9930
Luke Nihlen <luke.nihlen@gmail.com>
2020-03-25 13:55:18 -0700

    move version to 10.15

--- .travis/before-script-osx.sh
+++ .travis/before-script-osx.sh
@@ -2,5 +2,5 @@
 
 cd $TRAVIS_BUILD_DIR/build
 
-cmake -G Xcode -DPYTHON_EXECUTABLE=`which python3` -DCMAKE_OSX_DEPLOYMENT_TARGET=10.10 ..
+cmake -G Xcode -DPYTHON_EXECUTABLE=`which python3` -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 ..
 
