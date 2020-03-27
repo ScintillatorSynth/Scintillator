eaa0a0594e97fc933d48fef4aa1873ecb5c418c0
Luke Nihlen <luke.nihlen@gmail.com>
2020-03-27 14:29:11 -0700

    use ghc_filesystem

--- src/base/FileSystem.hpp
+++ src/base/FileSystem.hpp
@@ -1,12 +1,11 @@
 #ifndef SRC_CORE_FILE_SYSTEM_HPP_
 #define SRC_CORE_FILE_SYSTEM_HPP_
 
-// MacOS didn't start fully supporting <filesystem> fully until 10.15
-// TODO: remove this hard-coding once the minimum required version of Scintillator is 10.15
+// TODO: MacOS didn't start fully supporting <filesystem> fully until 10.15. Remove this third-party library once the
+// minimum required version of Scintillator is 10.15
 #if(__APPLE__)
-#    define _LIBCPP_NO_EXPERIMENTAL_DEPRECATION_WARNING_FILESYSTEM
-#    include <experimental/filesystem>
-namespace fs = std::experimental::filesystem;
+#    include "ghc/filesystem.hpp"
+namespace fs = ghc::filesystem;
 #else
 #if __has_include(<filesystem>)
 #    include <filesystem>
