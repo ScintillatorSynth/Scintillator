90a44f9b736d3be455eef6508d1ca70e813c8571
Luke Nihlen <luke.nihlen@gmail.com>
2020-03-27 13:03:25 -0700

    switch macos back to experimental/filesystem

--- src/base/FileSystem.hpp
+++ src/base/FileSystem.hpp
@@ -1,6 +1,13 @@
 #ifndef SRC_CORE_FILE_SYSTEM_HPP_
 #define SRC_CORE_FILE_SYSTEM_HPP_
 
+// MacOS didn't start fully supporting <filesystem> fully until 10.15
+// TODO: remove this hard-coding once the minimum required version of Scintillator is 10.15
+#if(__APPLE__)
+#    define _LIBCPP_NO_EXPERIMENTAL_DEPRECATION_WARNING_FILESYSTEM
+#    include <experimental/filesystem>
+namespace fs = std::experimental::filesystem;
+#else
 #if __has_include(<filesystem>)
 #    include <filesystem>
 namespace fs = std::filesystem;
@@ -8,5 +15,6 @@ namespace fs = std::filesystem;
 #    include <experimental/filesystem>
 namespace fs = std::experimental::filesystem;
 #endif
+#endif  // __OSX__
 
 #endif // SRC_CORE_FILE_SYSTEM_HPP_
