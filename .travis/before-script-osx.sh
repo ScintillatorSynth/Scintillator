eab614fcec7beaf11b669c459169f5503e6217b3
Luke Nihlen <luke.nihlen@gmail.com>
2020-03-27 13:22:30 -0700

    try again on macos 10.15

--- .travis/before-script-osx.sh
+++ .travis/before-script-osx.sh
@@ -2,6 +2,6 @@
 
 cd $TRAVIS_BUILD_DIR/build
 
-cmake -G Xcode -DPYTHON_EXECUTABLE=`which python3` -DCMAKE_OSX_DEPLOYMENT_TARGET=10.14                                 \
+cmake -G Xcode -DPYTHON_EXECUTABLE=`which python3` -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15                                 \
     -DSCIN_SCLANG=$TRAVIS_HOME/sclang/SuperCollider/SuperCollider.app/Contents/MacOS/sclang ..
 
