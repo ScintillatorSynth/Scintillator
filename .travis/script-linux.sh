75b7cc571eaed3f3a87704b8f694602993ff0797
Luke Nihlen <luke.nihlen@gmail.com>
2020-03-28 23:10:00 -0700

    fail fast on travis test failures

--- .travis/script-linux.sh
+++ .travis/script-linux.sh
@@ -3,9 +3,9 @@
 cd $TRAVIS_BUILD_DIR/build
 
 make -j2 swiftshader-local-install
-make -j2 install
-make coverage_report
-make sclang_unit_tests
-make -j2 lintall
-make -j2 docs
+make -j2 install || exit 1
+make coverage_report || exit 2
+make sclang_unit_tests || exit 3
+make -j2 lintall || exit 4
+make -j2 docs || exit 5
 
