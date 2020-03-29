753956858d8a9afa02d16c9150608857a1fcfb2d
Luke Nihlen <luke.nihlen@gmail.com>
2020-03-28 23:33:02 -0700

    fix linting

--- src/base/FileSystem.hpp
+++ src/base/FileSystem.hpp
@@ -3,17 +3,17 @@
 
 // TODO: MacOS didn't start fully supporting <filesystem> fully until 10.15. Remove this third-party library once the
 // minimum required version of Scintillator is 10.15
-#if(__APPLE__)
+#if (__APPLE__)
 #    include "ghc/filesystem.hpp"
 namespace fs = ghc::filesystem;
 #else
-#if __has_include(<filesystem>)
-#    include <filesystem>
+#    if __has_include(<filesystem>)
+#        include <filesystem>
 namespace fs = std::filesystem;
-#else
-#    include <experimental/filesystem>
+#    else
+#        include <experimental/filesystem>
 namespace fs = std::experimental::filesystem;
-#endif
-#endif  // __OSX__
+#    endif
+#endif // __OSX__
 
 #endif // SRC_CORE_FILE_SYSTEM_HPP_
