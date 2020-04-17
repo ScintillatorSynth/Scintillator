3e85c2a64cd7c0c085e40e9357b985654e414181
Luke Nihlen <luke.nihlen@gmail.com>
2020-04-16 22:38:12 -0700

    fix builds upload dir

--- .travis/before-deploy-osx.sh
+++ .travis/before-deploy-osx.sh
@@ -5,5 +5,5 @@ tar czf $HOME/builds/scinsynth.app.$TRAVIS_COMMIT.tgz $TRAVIS_BUILD_DIR/bin/scin
 shashum -a 256 -b $HOME/builds/scinsynth.app.$TRAVIS_COMMIT.tgz > $HOME/builds/scinsynth.app.$TRAVIS_COMMIT.sha256
 export S3_URL='http://scintillator-synth-coverage.s3-website-us-west-1.amazonaws.com/builds/scinsynth.app.'$TRAVIS_COMMIT'.tgz'
 export FWD_HTML='<html><head><meta http-equiv="refresh" content="0; url='$S3_URL'" /></head></html>'
-echo $FWD_HTML > $HOME/builds/scinsynth.app-latest.html
+echo $FWD_HTML > $HOME/builds/scinsynth.app.latest.html
 
