22ec50b542834dc18ab5f25910b3159c72b018db
Luke Nihlen <luke.nihlen@gmail.com>
2020-04-17 13:16:15 -0700

    fixup macos tarball path, some work on code signing (#80)

--- .travis/before-deploy-osx.sh
+++ .travis/before-deploy-osx.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir -p $HOME/builds
-tar czf $HOME/builds/scinsynth.app.$TRAVIS_COMMIT.tgz $TRAVIS_BUILD_DIR/bin/scinsynth.app
+tar czf $HOME/builds/scinsynth.app.$TRAVIS_COMMIT.tgz $TRAVIS_BUILD_DIR/*
 shasum -a 256 -b $HOME/builds/scinsynth.app.$TRAVIS_COMMIT.tgz > $HOME/builds/scinsynth.app.$TRAVIS_COMMIT.sha256
 export S3_URL='http://scintillator-synth-coverage.s3-website-us-west-1.amazonaws.com/builds/scinsynth.app.'$TRAVIS_COMMIT'.tgz'
 export FWD_HTML='<html><head><meta http-equiv="refresh" content="0; url='$S3_URL'" /></head></html>'
