c900d86f4208188c27099494186cd1ef7d9fde71
Luke Nihlen <luke.nihlen@gmail.com>
2020-04-22 08:14:54 -0700

    fix ordering dependency in make install

--- .travis/script-linux.sh
+++ .travis/script-linux.sh
@@ -2,10 +2,9 @@
 
 cd $TRAVIS_BUILD_DIR/build
 
-make -j2 swiftshader-local-install
-make -j2 install || exit 1
+make -j install || exit 1
 make coverage_report || exit 2
 make sclang_unit_tests || exit 3
-make -j2 lintall || exit 4
-make -j2 docs || exit 5
+make -j lintall || exit 4
+make -j docs || exit 5
 
