92e1510459eb58577ba8b84752ea097753af2279
Luke Nihlen <luke.nihlen@gmail.com>
2020-04-22 11:09:20 -0700

    fix osc message test

--- .travis/before-install-linux.sh
+++ .travis/before-install-linux.sh
@@ -4,10 +4,12 @@
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys FABAEF95
 sudo add-apt-repository --yes ppa:supercollider/ppa
 sudo apt-get update
+
 # Install all the build and test dependencies
 sudo apt-get install --yes build-essential  cmake doxygen gperf imagemagick libwayland-dev libxcursor-dev libxi-dev    \
     libxinerama-dev libxrandr-dev pkg-config python3-distutils python3-yaml supercollider-ide xvfb zlib1g
 
+# Coverage and Linting requires clang-8
 if $DO_COVERAGE; then
     sudo apt-get install --yes clang-8 clang-format-8 libc++-8-dev libc++abi-8-dev llvm-8-dev libc++abi-8-dev llvm-8-dev
 fi
--- tools/run-sclang.py
+++ tools/run-sclang.py
@@ -19,7 +19,7 @@ def main(argv):
         env['DISPLAY'] = ':99.0'
         xvfb = subprocess.Popen(['Xvfb', ':99', '-ac', '-screen', '0', '1280x1024x24'])
 
-    print('sclang command line' + sclang_path + ' ' + ' '.join(argv[1:]))
+    print('sclang command line: ' + sclang_path + ' ' + ' '.join(argv[1:]))
     proc = subprocess.Popen([sclang_path] + argv[1:], stdout=subprocess.PIPE, env=env)
 
     timeout = 300
@@ -31,7 +31,7 @@ def main(argv):
         if time.time() > (start_time + timeout):
             output = error_string
             break
-        output = proc.stdout.read().decode('utf-8')
+        output = proc.stdout.read(1024).decode('utf-8')
         print(output, end="")
         if proc.poll() == None:
             time.sleep(0.1)
