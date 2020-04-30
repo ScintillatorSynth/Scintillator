c33c077677a92fa1630a4d956874f3faab1e5d92
Luke Nihlen <luke.nihlen@gmail.com>
2020-04-30 14:00:40 -0700

    deploy linux releases to s3 (#95)
    * deploy linux releases to s3
    
    * force deploy for testing
    
    * second attempt to trigger deploy on branch
    
    * add code signing script for osx

--- .travis/before-deploy-linux.sh
+++ .travis/before-deploy-linux.sh
@@ -5,11 +5,17 @@ if [ $DO_COVERAGE = true ]; then
     cp $TRAVIS_BUILD_DIR/build/src/scinsynth_coverage.json $HOME/artifacts
     cp -R $TRAVIS_BUILD_DIR/build/doc $HOME/artifacts
     cp -R $TRAVIS_BUILD_DIR/build/report $HOME/artifacts
-else
+elif [[ -z $TRAVIS_TAG ]]; then
     mkdir -p $HOME/builds
     cp $TRAVIS_BUILD_DIR/bin/scinsynth-x86_64.AppImage $HOME/builds/scinsynth-$TRAVIS_COMMIT-x86_64.AppImage
     shasum -a 256 -b $HOME/builds/scinsynth-$TRAVIS_COMMIT-x86_64.AppImage > $HOME/builds/scinsynth-$TRAVIS_COMMIT-x86_64.sha256
     export S3_URL='http://scintillator-synth-coverage.s3-website-us-west-1.amazonaws.com/builds/scinsynth-'$TRAVIS_COMMIT'-x86_64.AppImage'
     export FWD_HTML='<html><head><meta http-equiv="refresh" content="0; url='$S3_URL'" /></head></html>'
     echo $FWD_HTML > $HOME/builds/scinsynth-latest-x86_64.AppImage.html
+else
+    mkdir -p $HOME/releases/$TRAVIS_TAG
+    cp $TRAVIS_BUILD_DIR/bin/scinsynth-x86_64.AppImage $HOME/releases/$TRAVIS_TAG/.
+    cd $HOME/releases/$TRAVIS_TAG
+    gzip scinsynth-x86_64.AppImage
+    shasum -a 256 -b scinsynth-x86_64.AppImage.gz > scinsynth-x86_64.AppImage.gz.sha256
 fi
