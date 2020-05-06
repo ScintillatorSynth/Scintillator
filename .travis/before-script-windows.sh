9b8253ca41d1898bbabafa53fa8eaf371c995a48
Luke Nihlen <luke.nihlen@gmail.com>
2020-05-05 21:19:16 -0700

    fix path and turn off other builds

--- .travis/before-script-windows.sh
+++ .travis/before-script-windows.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # cmake -DPYTHON_EXECUTABLE=/c/Python38/python.exe ..
-cd $TRAVIS_BUILD_DIR/build
+cd $TRAVIS_BUILD_DIR
 python tools/fetch-binary-deps
 cd $TRAVIS_BUILD_DIR/build
 cmake -DSCIN_BUILD_DOCS=OFF ..
