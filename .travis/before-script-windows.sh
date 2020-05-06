0701ca6a3ee5228eab2d4e3e1416daa574a6e97d
Luke Nihlen <luke.nihlen@gmail.com>
2020-05-05 21:29:57 -0700

    pick right python

--- .travis/before-script-windows.sh
+++ .travis/before-script-windows.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# cmake -DPYTHON_EXECUTABLE=/c/Python38/python.exe ..
 cd $TRAVIS_BUILD_DIR
-python tools/fetch-binary-deps.py
+/c/Python38/python.exe tools/fetch-binary-deps.py
 cd $TRAVIS_BUILD_DIR/build
-cmake -DSCIN_BUILD_DOCS=OFF ..
+cmake -DSCIN_BUILD_DOCS=OFF -DPYTHON_EXECUTABLE=/c/Python38/python.exe ..
 
