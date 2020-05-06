19f0fbd06936a0face1ae9fa8fa0e1a2fadc02a6
Luke Nihlen <luke.nihlen@gmail.com>
2020-05-06 09:26:24 -0700

    remove parallelism from travis linux builds
    It seems the make -j commands are exhausting memory. Remove them.

--- .travis/script-linux.sh
+++ .travis/script-linux.sh
@@ -2,13 +2,13 @@
 
 cd $TRAVIS_BUILD_DIR/build
 
-make -j install || exit 1
+make install || exit 1
 
 if [ $DO_COVERAGE = true ]; then
     make coverage_report || exit 2
     make sclang_unit_tests || exit 3
-    make -j lintall || exit 4
-    make -j docs || exit 5
+    make lintall || exit 4
+    make docs || exit 5
 else
     make compare_images || exit 2
 fi
