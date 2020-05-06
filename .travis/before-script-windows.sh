3cca8b21ec9360ebec913ba716059103e9ebb86b
Luke Nihlen <luke.nihlen@gmail.com>
2020-05-05 21:24:42 -0700

    fetch-deps.py

--- .travis/before-script-windows.sh
+++ .travis/before-script-windows.sh
@@ -2,7 +2,7 @@
 
 # cmake -DPYTHON_EXECUTABLE=/c/Python38/python.exe ..
 cd $TRAVIS_BUILD_DIR
-python tools/fetch-binary-deps
+python tools/fetch-binary-deps.py
 cd $TRAVIS_BUILD_DIR/build
 cmake -DSCIN_BUILD_DOCS=OFF ..
 
