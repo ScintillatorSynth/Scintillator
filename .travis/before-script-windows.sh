389c0991135802883ac81991cc1af240dfebdfe6
Luke Nihlen <luke.nihlen@gmail.com>
2020-05-07 11:11:10 -0700

    hard-code path to sclang binary

--- .travis/before-script-windows.sh
+++ .travis/before-script-windows.sh
@@ -3,5 +3,5 @@
 cd $TRAVIS_BUILD_DIR
 /c/Python38/python.exe tools/fetch-binary-deps.py
 cd $TRAVIS_BUILD_DIR/build
-cmake -DSCIN_BUILD_DOCS=OFF -DPYTHON_EXECUTABLE=/c/Python38/python.exe -DCMAKE_GENERATOR_PLATFORM=x64 ..
+cmake -DSCIN_BUILD_DOCS=OFF -DSCIN_SCLANG=/c/Program\ Files/SuperCollider-3.9.3/sclang.exe -DPYTHON_EXECUTABLE=/c/Python38/python.exe -DCMAKE_GENERATOR_PLATFORM=x64 ..
 
