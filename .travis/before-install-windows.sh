a34a4779a6b70fe90938040374e5c6a954a16a83
Luke Nihlen <luke.nihlen@gmail.com>
2020-05-08 13:47:27 -0700

    try downloading and installing vulkan rumtime

--- .travis/before-install-windows.sh
+++ .travis/before-install-windows.sh
@@ -7,6 +7,14 @@ git clone https://github.com/yaml/pyyaml.git pyyaml
 cd pyyaml
 /c/Python38/python.exe setup.py install
 
+cd $HOME
+mkdir vulkan
+cd vulkan
+curl https://scintillator-synth-coverage.s3-us-west-1.amazonaws.com/dependencies/windows/vulkan-runtime.exe --output vulkan-runtime.exe
+./vulkan-runtime.exe /S
+
 cd $TRAVIS_BUILD_DIR
 /c/Python38/python.exe tools/fetch-binary-deps.py
 /c/Python38/python.exe tools/fetch-sclang.py $TRAVIS_HOME/sclang
+
+
