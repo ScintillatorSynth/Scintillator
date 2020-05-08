6576588983dae9f7bd5b19edf16c86d673054361
Luke Nihlen <luke.nihlen@gmail.com>
2020-05-07 23:36:58 -0700

    add ps calls to detect stuck processes

--- .travis/script-windows.sh
+++ .travis/script-windows.sh
@@ -4,4 +4,15 @@ touch $TRAVIS_BUILD_DIR/bin/disable_auto_install
 cd $TRAVIS_BUILD_DIR/build
 cmake --build . --config Release
 cmake --install . --config Release
+echo "building language config"
+cmake --build . --config Release --target $TRAVIS_BUILD_DIR/build/testing/sclang_config.yaml
+echo "building test images"
+ps
+cmake --build . --config Release --target test_images
+echo "comparing images"
+ps
 cmake --build . --config Release --target compare_images
+
+
+
+
