dab92e46464ca18d061cc7f101b206e367c3bdcf
Luke Nihlen <luke.nihlen@gmail.com>
2020-05-08 08:10:37 -0700

    make language config an explicit target

--- .travis/script-windows.sh
+++ .travis/script-windows.sh
@@ -4,15 +4,15 @@ touch $TRAVIS_BUILD_DIR/bin/disable_auto_install
 cd $TRAVIS_BUILD_DIR/build
 cmake --build . --config Release
 cmake --install . --config Release
+
 echo "building language config"
-cmake --build . --config Release --target $TRAVIS_BUILD_DIR/build/testing/sclang_config.yaml
+cmake --build . --config Release --target sclang_language_config
+ps
+
 echo "building test images"
-ps
 cmake --build . --config Release --target test_images
-echo "comparing images"
 ps
+
+echo "comparing images"
 cmake --build . --config Release --target compare_images
-
-
-
-
+ps
