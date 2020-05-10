edee8e04c4a073ff37305ee21eb72fd730230629
Luke Nihlen <luke.nihlen@gmail.com>
2020-05-10 09:26:04 -0700

    change argument to cygpath for windows paths

--- .travis/script-windows.sh
+++ .travis/script-windows.sh
@@ -6,7 +6,7 @@ cmake --install . --config Release
 
 echo "Configuring host machine to use Swiftshader as a Vulkan device"
 powershell 'New-Item -Path "HKLM:\SOFTWARE\Khronos\Vulkan" -Name Drivers'
-export SWSHADER_PATH=`cygpath -d $TRAVIS_BUILD_DIR`'\bin\scinsynth-w64\vulkan\icd.d\vk_swiftshader_icd.json'
+export SWSHADER_PATH=`cygpath -w $TRAVIS_BUILD_DIR`'\bin\scinsynth-w64\vulkan\icd.d\vk_swiftshader_icd.json'
 echo "Setting Swiftshader path to $SWSHADER_PATH"
 powershell 'New-ItemProperty -PATH "HKLM:\SOFTWARE\Khronos\Vulkan\Drivers" -Name "'$SWSHADER_PATH'" -Value "0" -PropertyType "DWORD"'
 
