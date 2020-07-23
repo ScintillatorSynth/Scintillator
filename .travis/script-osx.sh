fd93adcb93c6c8272cd1f935ed2a010669b489d0
Luke Nihlen <luke.nihlen@gmail.com>
2020-07-23 16:55:29 -0700

    change install command to relwithdebinfo

--- .travis/script-osx.sh
+++ .travis/script-osx.sh
@@ -5,6 +5,6 @@ XCPRETTY='xcpretty --simple --no-utf --no-color'
 cd $TRAVIS_BUILD_DIR/build
 cmake --build . --config RelWithDebInfo | $XCPRETTY || exit 1
 cmake --build . --config RelWithDebInfo --target dump_symbols || exit 2
-cmake --install . || exit 3
+cmake --install . --config RelWithDebInfo || exit 3
 cmake --build . --target compare_images --config RelWithDebInfo || exit 4
 
