73b1202ab7165b5e9da6a6185b25fabeceb70ee5
Luke Nihlen <luke.nihlen@gmail.com>
2020-07-23 15:58:24 -0700

    actually enable crashpad in build

--- .travis/before-script-osx.sh
+++ .travis/before-script-osx.sh
@@ -4,7 +4,6 @@ touch $TRAVIS_BUILD_DIR/bin/disable_auto_install
 cd $TRAVIS_BUILD_DIR/build
 
 cmake -G Xcode                                                                                                         \
-    -DSCIN_USE_CRASHPAD=OFF                                                                                            \
     -DPYTHON_EXECUTABLE=`which python3`                                                                                \
     -DSCIN_BUILD_DOCS=OFF                                                                                              \
     -DCMAKE_OSX_DEPLOYMENT_TARGET=10.13                                                                                \
--- .travis/script-osx.sh
+++ .travis/script-osx.sh
@@ -3,8 +3,8 @@
 XCPRETTY='xcpretty --simple --no-utf --no-color'
 
 cd $TRAVIS_BUILD_DIR/build
-cmake --build . --config RelWithDebInfo | $XCPRETTY
-cmake --build . --config RelWithDebInfo --target dump_symbols
-cmake --install .
-cmake --build . --target compare_images --config RelWithDebInfo
+cmake --build . --config RelWithDebInfo | $XCPRETTY || exit 1
+cmake --build . --config RelWithDebInfo --target dump_symbols || exit 2
+cmake --install . || exit 3
+cmake --build . --target compare_images --config RelWithDebInfo || exit 4
 
