ed60d2341b25deb25b5ed3a3a3e64a3173b398e4
Luke Nihlen <luke.nihlen@gmail.com>
2020-07-30 10:44:44 -0700

    try switching back to gnu stdlibc++

--- .travis/before-script-linux.sh
+++ .travis/before-script-linux.sh
@@ -5,10 +5,10 @@ cd $TRAVIS_BUILD_DIR/build
 
 if [ $DO_COVERAGE = true ]; then
     cmake -DSCIN_BUILD_DOCS=ON -DCMAKE_BUILD_TYPE=Coverage -DLLVM_COV="llvm-cov-8" -DLLVM_PROFDATA="llvm-profdata-8"   \
-        -DPYTHON_EXECUTABLE=`which python3` -DSCIN_SCLANG=`which sclang` -DCMAKE_CXX_FLAGS="-stdlib=libc++"            \
-        -DSCIN_USE_LIBCXX=ON ..
+        -DPYTHON_EXECUTABLE=`which python3` -DSCIN_SCLANG=`which sclang` -DCMAKE_CXX_FLAGS="-stdlib=libstdc++"         \
+        -DSCIN_USE_LIBCXX=OFF ..
 else
     cmake -DSCIN_BUILD_DOCS=OFF -DCMAKE_BUILD_TYPE=RelWithDebInfo -DPYTHON_EXECUTABLE=`which python3`                  \
-        -DSCIN_SCLANG=`which sclang` -DCMAKE_CXX_FLAGS="-stdlib=libc++" -DSCIN_USE_LIBCXX=ON ..
+        -DSCIN_SCLANG=`which sclang` -DCMAKE_CXX_FLAGS="-stdlib=libstdc++" -DSCIN_USE_LIBCXX=OFF ..
 fi
 
