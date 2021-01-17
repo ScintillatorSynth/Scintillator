f1b9205e7c5ba2cc539acd82a60623b29547dfe8
Luke Nihlen <luke.nihlen@gmail.com>
2021-01-17 13:15:17 -0800

    fix python path on windows Travis build

--- .travis/before-script-windows.sh
+++ .travis/before-script-windows.sh
@@ -4,6 +4,6 @@ touch $TRAVIS_BUILD_DIR/bin/disable_auto_install
 cd $TRAVIS_BUILD_DIR/build
 cmake -DSCIN_BUILD_DOCS=OFF                                                                                            \
       -DSCIN_SCLANG=$TRAVIS_HOME/sclang/SuperCollider/sclang.exe                                                       \
-      -DPYTHON_EXECUTABLE=/c/Python38/python.exe                                                                       \
+      -DPYTHON_EXECUTABLE=/c/Python39/python.exe                                                                       \
       -DCMAKE_GENERATOR_PLATFORM=x64                                                                                   \
       ..
